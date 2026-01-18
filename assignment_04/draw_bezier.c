53e3d07e93055442e1c5e9e154f57a2eb24967ec
HynekS <hyneksvacha@seznam.cz>
2026-01-18 20:34:29 +0100

    WIP1

--- assignment_04/draw_bezier.c
+++ assignment_04/draw_bezier.c
@@ -51,12 +51,44 @@ float get_midpoint(float current, float next, int steps, int step) {
   return midpoint;
 }
 
-point* recursion(point* points, int line_count, int steps) {
-  if (line_count == 1) return points;
+point *reduce_lines(point **points, int line_count, int steps) {
+  if (line_count == 1)
+    return points[0];
 
-  point midpoints[line_count - 1][steps];
+  point **midpoints = (point **)malloc((line_count - 1) * steps * sizeof(point *));
+  if (midpoints == NULL) {
+    fprintf(stderr, "Error: Failed to allocate memory for midpoints.\n");
+    exit(1);
+  }
 
   for (int i = 0; i < line_count - 1; i++) {
+    // midpoints[i] = (point *)malloc(steps * sizeof(point));
+
+    for (int step = 0; step < steps; step++) {
+      int x_current = points[i][step].x;
+      int y_current = points[i][step].y;
+
+      int x_next = points[i + 1][step].x;
+      int y_next = points[i + 1][step].y;
+
+      midpoints[i][step].x =
+          get_midpoint((float)x_current, (float)x_next, steps, step);
+      midpoints[i][step].y =
+          get_midpoint((float)y_current, (float)y_next, steps, step);
+    }
+  }
+
+  return reduce_lines(midpoints, line_count - 1, steps);
+}
+
+point** create_steps(point *points, int points_count, int steps) {
+  point **midpoints = (point **)malloc((points_count - 1) * steps * sizeof(point *));
+  if (midpoints == NULL) {
+    fprintf(stderr, "Error: Failed to allocate memory for midpoints.\n");
+    exit(1);
+  }
+
+  for (int i = 0; i < points_count - 1; i++) {
     int x_current = points[i].x;
     int y_current = points[i].y;
 
@@ -71,13 +103,13 @@ point* recursion(point* points, int line_count, int steps) {
     }
   }
 
-  return recursion(midpoints, line_count - 1, steps);
+  return midpoints;
 }
 
 // void draw_bezier(int (*points)[2], int points_count, int steps, PGMImage
 // *canvas) {
 void draw_bezier(point *points, int points_count, int steps, PGMImage *canvas) {
-  //float midpoints[points_count - 1][steps][2];
+  // float midpoints[points_count - 1][steps][2];
   point midpoints[points_count - 1][steps];
 
   for (int i = 0; i < points_count - 1; i++) {
@@ -120,10 +152,17 @@ int main() {
   PGMImage *blank_canvas = create_blank_image(256, 256);
 
   // int points[5][2] = {{1, 1}, {32, 120}, {128, 160}, {164, 16}, {256, 0}};
-  point points[5] = {
+  int const points_count = 5;
+  int const steps = 32;
+  point points[points_count] = {
       {1.0, 1.0}, {32.0, 120.0}, {128.0, 160.0}, {164.0, 16.0}, {256.0, 0.0}};
 
-  draw_bezier(points, sizeof(points) / sizeof(points[0]), 48, blank_canvas);
+  // draw_bezier(points, sizeof(points) / sizeof(points[0]), 48, blank_canvas);
+  // draw_bezier(points, sizeof(points) / sizeof(points[0]), 48, blank_canvas);
+  point** foo = create_steps(points, points_count, steps);
+
+  point* bar = reduce_lines(foo, points_count - 1, steps);
+
 
   pgma_write("bezier.pgm", "Output bezier curve drawing", blank_canvas->xsize,
              blank_canvas->ysize, blank_canvas->maxg, blank_canvas->g);
