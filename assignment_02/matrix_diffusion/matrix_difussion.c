fbd7a6183a061ea6399b1530ca26802bf547d00c
HynekS <hyneksvacha@seznam.cz>
2026-01-29 19:28:45 +0100

    Attempt to fix math

--- assignment_02/matrix_diffusion/matrix_difussion.c
+++ assignment_02/matrix_diffusion/matrix_difussion.c
@@ -1,54 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-char* four_from_one(char element) {
- 
-}
-
-
 char *create_difussion_matrix(char *matrix, char current_dimension,
                               char max_dimension) {
-
-  printf("current_dimension: %d\n", current_dimension);
   if (current_dimension >= max_dimension)
     return matrix;
 
-  char extended_matrix_item_count = (current_dimension << 2);
-  printf("extended_matrix_item_count: %d\n", extended_matrix_item_count);
+  char extended_matrix_item_count = (current_dimension * 4);
 
   char *extended_matrix = malloc(extended_matrix_item_count * sizeof(char));
 
-  int current_op = 0;
-
-  for (int i = 0; i < extended_matrix_item_count; i++) {
+  for (int i = 0; i < extended_matrix_item_count / 4; i++) {
     char current_matrix_item = matrix[i];
-    // TO FIX
-    char current_op = extended_matrix_item_count / 4;
+    for (int current_op = 0; current_op < 4; current_op++) {
+      int extended_matrix_index = (i * 4) + current_op;
 
-    printf("current_op: %d\n", current_op);
-    if (current_op == 0) {
-      extended_matrix[i] = current_matrix_item * 4;
-    }
+      if (current_op == 0) {
+        extended_matrix[extended_matrix_index] = (current_matrix_item * 4);
+      }
 
-    if (current_op == 1) {
-      extended_matrix[i] = (current_matrix_item * 4) + 3;
-    }
+      if (current_op == 1) {
+        extended_matrix[extended_matrix_index] = (current_matrix_item * 4) + 3;
+      }
 
-    if (current_op == 2) {
-      extended_matrix[i] = (current_matrix_item * 4) + 2;
-    }
+      if (current_op == 2) {
+        extended_matrix[extended_matrix_index] = (current_matrix_item * 4) + 2;
+      }
 
-    if (current_op == 3) {
-      extended_matrix[i] = (current_matrix_item * 4) + 1;
+      if (current_op == 3) {
+        extended_matrix[extended_matrix_index] = (current_matrix_item * 4) + 1;
+      }
     }
   }
-
-  printf("[");
-  for (int k = 0; k < extended_matrix_item_count; k++) {
-    printf("%d, ", extended_matrix[k]);
-  }
-  printf("]\n");
 
   free(matrix);
 
@@ -60,6 +43,13 @@ int main() {
   char *base_matrix = malloc(sizeof(char));
   base_matrix[0] = 0;
 
-  char *m = create_difussion_matrix(base_matrix, 1, 16);
+  char *m = create_difussion_matrix(base_matrix, 1, 64);
+
+  printf("[");
+  for (int k = 0; k < 256; k++) {
+    printf("%d, ", m[k]);
+  }
+  printf("]\n");
+
   free(m);
 }
